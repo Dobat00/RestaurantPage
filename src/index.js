b98fe972e000fc03453134d91cc0dbbff98580e7
matheus kenzo <kenzoyamashitaasano@gmail.com>
2023-08-15 18:25:13 -0300

    inicio da construcao da homepage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-console.log('funcionando')
-const content = document.getElementById('content')
-let element = document.createElement('h1')
-element.textContent = 'Restaurante X'
-content.appendChild(element)
+
+import createHome from "./home.js";
+
+const content = document.getElementById('content');
+content.appendChild(createHome())
