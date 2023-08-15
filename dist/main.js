6943ef44041c26c979cc02d82be2b32875e911dc
matheus kenzo <kenzoyamashitaasano@gmail.com>
2023-08-14 21:53:27 -0300

    webpack instalado, primeiros elementos criados

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{console.log("funcionando");const e=document.getElementById("content");let t=document.createElement("h1");t.textContent="Restaurante X",e.appendChild(t)})();
---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log('funcionando')
+const content = document.getElementById('content')
+let element = document.createElement('h1')
+element.textContent = 'Restaurante X'
+content.appendChild(element)
